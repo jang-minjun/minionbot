0b2fff163280947546a7fc655da41e7de3922ccd
jang-minjun <47943498+jang-minjun@users.noreply.github.com>
2019-02-24 22:04:26 +0900

    Add files via upload

---
+++ minionBOT.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("loigin")
+    print(client.user.name)
+    print(client.user.id)
+    print("-------------------")
+    await client.change_presence(game=discord.Game(name='바나나 먹는중..', type=1))
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('안녕 미니모'):
+        await client.send_message(message.channel, "..안뇽")
+    if message.content.startswith('미니모!'):
+        await client.send_message(message.channel, "바냐냐 ㅎㅎ")
+    if message.content.startswith('바나나?'):
+        await client.send_message(message.channel, "미니모!")
+    if message.content.startswith('출첵!'):
+        await client.send_message(message.channel, "{@name}출첵 하셨습니다! :banana:")
+
+client.run('NTQ5MTgwMDExMzM3MzUxMTY4.D1QHtA.Jxis0SJEFj0aKTPNOc52TRjJFVA')
+
